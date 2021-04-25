31b16c2fc3ee84d056d4ff50d8ce47299382089d
Jhonathan O'Brian Saa Se <64106872+InterTech20@users.noreply.github.com>
2021-04-25 00:01:44 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-let date=new Date();("longitud");
+/*
 
+Página desarrollada  por Jhonathan O'brian Saavedra web's, subida y alojada en github para que los desarrolladores puedan ver el codigo y usarlo para test. 
+En Inter Tech ayudamos los jóvenes que van empezando. Siguenos en Facebook: https://www.facebook.com/InteraccionTecnologica y 
+github como:https://github.com/intertech20/ 
+
+*/
+
+/* ==============================================
+                    VARIABLES
+ ==============================================*/
+let date=new Date();
+
+let btn3=document.querySelector('.item_slider3');
+let btn2=document.querySelector('.item_slider2');
+let btn1=document.querySelector('.item_slider1');
+
+/* ==============================================
+             COODENADAS DEL NAVEGADOR
+ ==============================================*/
 var options = {
     enableHighAccuracy: true,
     timeout: 5000,
@@ -17,9 +35,11 @@ var options = {
   
   navigator.geolocation.getCurrentPosition(success, error, options);
 
-  
-// ---------------------------------------------
-
+/* ==============================================
+              CAPTURANDO LAS COORDENADAS
+              SE CREA EL MAPS SE CONSUME
+                 LA API LEAFLETJS
+ ==============================================*/
   function create_map(latitude,longitude) {
     let mymap = L.map('map').setView([latitude,longitude], 13);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
@@ -31,105 +51,168 @@ var options = {
     L.marker([latitude, longitude]).addTo(mymap);
 }
 
-//--------------------------------------------------
-
+/* ==============================================
+              CAPTURANDO LAS COORDENADAS
+              SE CREA EL MAPS SE CONSUME
+                LA API OPENWEATHERMAP
+ ==============================================*/
+ 
+ 
 function create_temp(latitude,longitude){
-
-
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=87e4f01705095dec0164761d3cb63252&units=metric&lang=es`).then(response => response.json())
          .then(data => {
-          document.querySelector("#map_marker").textContent=data.name;
+  
+        create_temp_function("#map_marker",data.name);
 
-          document.querySelector(".date").textContent=date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear()+" |  "+date.getHours()+":"+date.getMinutes();
+        create_temp_function(".date",date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear()+" |  "+date.getHours()+":"+date.getMinutes());
+         
+        create_temp_function(".data_clima_item1",
+          `<span class="img_description text_clima_item_left">${data.weather[0]["description"]}</span>
+          <img class="temp-icon text_clima_item_right" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${data.weather[0]["icon"]}.svg" 
+          title="${data.weather[0]["description"]}"
+          alt="${data.weather[0]["description"]}">
+           `);
+          // ---------------------data_clima_contaniner_item1------------------------
+        create_temp_function(".data_clima_item2",`<p class="text_clima_item_left">Temperatura</p><p class="text_clima_item_right">${Math.round(data.main.temp)} ° C</p>`);
+   
 
-          document.querySelector(".data_clima_item2").textContent=Math.round(data.main.temp)+"° C";
-          document.getElementById("temp_max").innerHTML=Math.round(data.main.temp_max)+"Max";
-          document.getElementById("temp_min").innerHTML=Math.round(data.main.temp_min)+"Min";
+        create_temp_function(".data_clima_item3",`<p class="text_clima_item_left">Temperatura Máxima</p><p class="text_clima_item_right">${Math.round(data.main.temp_max)} ° C</p>`);
+   
+        create_temp_function(".data_clima_item4",`<p class="text_clima_item_left">Temperatura Minima</p><p class="text_clima_item_right">${Math.round(data.main.temp_min)} ° C</p>`);
 
-          document.querySelector(".data_clima_item4").textContent=data.main.humidity+" Humedad";
+        create_temp_function(".data_clima_item5",`<p class="text_clima_item_left">Humedad</p><p class="text_clima_item_right">${data.main.humidity}</p>`);
+
+        create_temp_function(".data_clima_item6",`<p class="text_clima_item_left">Presión</p><p class="text_clima_item_right">${data.main.pressure}</p>`);
+
+        create_temp_function(".data_clima_item7",`<p class="text_clima_item_left">Nivel del suelo</p><p class="text_clima_item_right">${data.main.grnd_level}</p>`);
       
+        create_temp_function(".data_clima_item8",`<p class="text_clima_item_left">Ráfaga</p><p class="text_clima_item_right">${data.wind.gust}</p>`);
 
-          document.querySelector(".data_clima_item5").textContent=data.main.pressure+" presión";
-
-
-
-document.querySelector(".data_clima_item6").textContent=data.main.grnd_level+"ground level";
-
-document.querySelector(".data_clima_item7").textContent=data.wind.gust+"gust";
-
-document.querySelector(".data_clima_item8").textContent=data.wind.speed+"speed"
+        create_temp_function(".data_clima_item9",`<p class="text_clima_item_left">Velocidad</p><p class="text_clima_item_right">${data.wind.speed}</p>`);
 
              pronostico_5_days(data.name);
         }).catch(error => console.log(error)); 
 
  }
 
+ function create_temp_function(etiqueta,string_) {
+    document.querySelector(etiqueta).innerHTML=string_;
+ }
 
+/* ==============================================
+    CAPTURANDO LAS COORDENADAS SE CREA EL MAPS 
+        SE CONSUME LA API OPENWEATHERMAP PARA 
+              CAPTURAR EL PRONONOSTICO
+                DEL CLIMA EN 5 DIAS
+ ==============================================*/
  function pronostico_5_days(params) {
-     console.log(params);
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${params}&appid=87e4f01705095dec0164761d3cb63252&units=metric&lang=es`).then(response => response.json())
      .then(data => {
-     
+     date_pronostico(data);
   }).catch(error => console.log(error)); 
  }
 
- /********************************/
- let btn3=document.querySelector('.item_slider3');
- let btn2=document.querySelector('.item_slider2');
-let btn1=document.querySelector('.item_slider1');
-btn1.addEventListener('click',function () {
-  document.querySelector('.data_clima_contaniner').classList.toggle('transition1');
-  document.querySelector('.data_clima_contaniner').classList.remove('transition2');
-  document.querySelector('.data_clima_contaniner').classList.remove('transition3');
-  btn1.classList.toggle('owl');
-  btn2.classList.remove('owl');
-  btn3.classList.remove('owl');
-  console.log("primero");
-});
+/* ==============================================
+            SE ENVIAN LOS VALORES CAPTURADOS
+            DEL PRONOSTICO A SU RESPECTIVAS 
+                      ETIQUETAS
+ ==============================================*/
+ 
+ function date_pronostico(data) {
+      //---------------------------------------- 
+      date_pronostico_function (1,(data.list[2].dt_txt).slice(0, 10),
+      Math.round(data.list[2].main.temp_max),
+      Math.round(data.list[2].main.temp_min));
+      //----------------------------------------
+      date_pronostico_function (2,(data.list[9].dt_txt).slice(0, 10),
+      Math.round(data.list[9].main.temp_max),
+      Math.round(data.list[9].main.temp_min));
+     //----------------------------------------
+      date_pronostico_function (3,(data.list[19].dt_txt).slice(0, 10),
+      Math.round(data.list[19].main.temp_max),
+      Math.round(data.list[19].main.temp_min));
+      //----------------------------------------
+      date_pronostico_function (4,(data.list[24].dt_txt).slice(0, 10),
+      Math.round(data.list[24].main.temp_max),
+      Math.round(data.list[24].main.temp_min));
+      //----------------------------------------
+      date_pronostico_function (5,(data.list[31].dt_txt).slice(0, 10),
+      Math.round(data.list[31].main.temp_max),
+      Math.round(data.list[31].main.temp_min));
+      //---------------------------------------- 
+      hour_pronostico(data);
+ }
 
+ function date_pronostico_function (number,data_1,data_2,data_3){
+  document.querySelector(".date_pronostico_"+number).innerHTML=`
+   <p class="text_clima_item_left">${data_1}</p>
+   <p class="text_clima_item_right">
+   <i class="fas fa-long-arrow-alt-up"></i>
+   ${data_2} ° C</p>
+   <p class="text_clima_item_right">
+   <i class="fas fa-long-arrow-alt-down"></i>
+   ${data_3} ° C</p>`
+ }
+
+ /* ==============================================
+            SE ENVIAN LOS VALORES CAPTURADOS
+            DEL PRONOSTICO A SU RESPECTIVAS 
+                      ETIQUETAS
+ ==============================================*/
+ 
+ function hour_pronostico(data) {
+   hour_pronostico_function (1,(data.list[2].dt_txt).slice(11, 16),
+   Math.round(data.list[2].main.temp));
+    //----------------------------------------
+    hour_pronostico_function (2,(data.list[9].dt_txt).slice(11, 16),
+    Math.round(data.list[9].main.temp));
+     //----------------------------------------
+    hour_pronostico_function (3,(data.list[19].dt_txt).slice(11, 16),
+    Math.round(data.list[19].main.temp));
+    //----------------------------------------
+    hour_pronostico_function (4,(data.list[24].dt_txt).slice(11, 16),
+    Math.round(data.list[24].main.temp));
+    //----------------------------------------
+    hour_pronostico_function (5,(data.list[31].dt_txt).slice(11, 16),
+    Math.round(data.list[31].main.temp));
+     //---------------------------------------- 
+    //  let dat = new Date(2021,3,25);
+    //  console.log((dat.toString()).slice(0, 11));
+ }
+
+ function hour_pronostico_function (number,data_1,data_2){
+    document.querySelector(".hour_pronostico_"+number).innerHTML=`
+    <p class="text_clima_item_left">${data_1}</p>
+    <p class="text_clima_item_right">${data_2} ° C</p>`;
+ }
+
+/* ==============================================
+              BOTONES DEL SLIDER
+ ==============================================*/
+btn1.addEventListener('click',function () {
+    data_clima_contaniner_fuction('transition2','transition1','transition3');
+    button(btn1,btn2,btn3);
+
+});
 
 btn2.addEventListener('click',function () {
-  document.querySelector('.data_clima_contaniner').classList.toggle('transition2');
-  document.querySelector('.data_clima_contaniner').classList.remove('transition1');
-  document.querySelector('.data_clima_contaniner').classList.remove('transition3');
-  btn1.classList.remove('owl');
-  btn2.classList.toggle('owl');
-  btn3.classList.remove('owl');
+    data_clima_contaniner_fuction('transition2','transition1','transition3');
+    button(btn2,btn1,btn3);
 });
-
 
 btn3.addEventListener('click',function () {
-  document.querySelector('.data_clima_contaniner').classList.toggle('transition3');
-  document.querySelector('.data_clima_contaniner').classList.remove('transition2');
-  document.querySelector('.data_clima_contaniner').classList.remove('transition1');
-  btn1.classList.remove('owl');
-  btn2.classList.remove('owl');
-  btn3.classList.toggle('owl');
+    data_clima_contaniner_fuction('transition3','transition2','transition2');
+    button(btn3,btn1,btn2);
 });
 
+function data_clima_contaniner_fuction(transition1,transition2,transition3) {
+    document.querySelector('.data_clima_contaniner').classList.toggle(transition1);
+    document.querySelector('.data_clima_contaniner').classList.remove(transition2);
+    document.querySelector('.data_clima_contaniner').classList.remove(transition3);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-Llame a los datos meteorológicos actuales para una ubicación
-=>https://api.openweathermap.org/data/2.5/weather?lat=${$latitud.innerHTML}&lon=${$longitud.innerText}&appid=87e4f01705095dec0164761d3cb63252
-
-Pronóstico del tiempo de 5 días
-=>api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
-
-
-https://www.google.com/maps/place/4%C2%B055'16.9%22S+80%C2%B039'57.5%22W/@-4.9213496,-80.6681674,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d-4.9213549!4d-80.6659734
-*/
+function button(tog,rem_1,rem_2) {
+    tog.classList.toggle('owl');
+    rem_1.classList.remove('owl');
+    rem_2.classList.remove('owl');
+}
